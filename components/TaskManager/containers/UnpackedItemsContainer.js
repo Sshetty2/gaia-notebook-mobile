d4ec8e28e42c0907cedac54ef6c210f49963ad49
Sachit <sachit.shetty@gmail.com>
2019-03-06 11:33:05 -0500

    add task manager components and containers

---
+++ components/TaskManager/containers/UnpackedItemsContainer.js
@@ -0,0 +1,22 @@
+import { connect } from 'react-redux';
+import Items from '../components/Items';
+
+import {
+  toggleItem,
+  removeItem,
+} from '../actions/items-actions'
+
+const mapStateToProps = (state) => {
+  return { items: state.items.present.filter(item => !item.packed && item.value.includes(state.filter.unpackedItemsFilter)) };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  onCheckOff(id) {
+    dispatch(toggleItem(id))
+  },
+  onRemove(id) {
+    dispatch(removeItem(id))
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Items);
