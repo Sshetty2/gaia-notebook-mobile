88eaa16f68f9b474538c94470c89431650adc594
Sachit <sachit.shetty@gmail.com>
2019-03-06 15:04:12 -0500

    update imports

--- reducers/new-item-reducer.js
+++ reducers/new-item-reducer.js
@@ -1,4 +1,4 @@
-import { UPDATE_NEW_ITEM_VALUE } from '../constants';
+import { UPDATE_NEW_ITEM_VALUE } from './constants';
 
 export default function(state = '', action) {
   if (action.type === UPDATE_NEW_ITEM_VALUE) {
