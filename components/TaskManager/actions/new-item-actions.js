79fe8105969065daa6eea794eac7dcf5570de864
Sachit <sachit.shetty@gmail.com>
2019-03-06 16:49:15 -0500

    update import

--- components/TaskManager/actions/new-item-actions.js
+++ components/TaskManager/actions/new-item-actions.js
@@ -1,4 +1,4 @@
-import { UPDATE_NEW_ITEM_VALUE } from '../components/TaskManager/constants';
+import { UPDATE_NEW_ITEM_VALUE } from '../../TaskManager/constants';
 
 export const updateNewItemValue = (value) => ({
   type: UPDATE_NEW_ITEM_VALUE,
