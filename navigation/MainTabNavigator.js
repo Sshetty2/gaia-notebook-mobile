5b867b61108703e64ff3f55051a7f5550cac3ecb
Sachit <sachit.shetty@gmail.com>
2019-03-06 11:47:00 -0500

    update stack navigator to render taskmanager screen instead of out-of-box home screen

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Platform } from 'react-native';
 import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
 
+import TaskManager from '../screens/TaskManager'
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 
 const HomeStack = createStackNavigator({
-  Home: HomeScreen,
+  Home: TaskManager,
 });
 
 HomeStack.navigationOptions = {
